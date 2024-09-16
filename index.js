f957e15c79828dc1b470c2be9fc79282f9a95236
lifehackerhansol <lifehackerhansol@ds-homebrew.com>
2024-09-16 09:02:25 -0700

    config: add FontAwesome
    For that sweet magnet icon for torrent magnet links

--- index.js
+++ index.js
@@ -7,11 +7,20 @@ import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
 
+/* FontAwesome stuff */
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faMagnet } from '@fortawesome/free-solid-svg-icons'
+library.add(faMagnet)
+
 /** @type {import('vitepress').Theme} */
 export default {
   extends: DefaultTheme,
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
     })
+  },
+  enhanceApp({ app, router, siteData }) {
+    app.component('font-awesome-icon', FontAwesomeIcon)
   }
 }
