eae7a1dfe2bdbb336a7170859eaaad2198bd9e18
lifehackerhansol <lifehackerhansol@ds-homebrew.com>
2025-08-09 08:51:49 -0700

    components: update to VitePress v2.0.0-alpha.9

--- components/VPFooter.vue
+++ components/VPFooter.vue
@@ -1,6 +1,6 @@
 <!--
-  Copyright (c) 2019-2024 Yuxi (Evan) You
-  Copyright (c) 2024 Nintendo Homebrew
+  Copyright (c) 2019-2025 Yuxi (Evan) You
+  Copyright (c) 2024-2025 Nintendo Homebrew
 
   SPDX-License-Identifier: MIT
 -->
